fec4c85bcb8ad997903cdebcf39c5999a4baaac3
Ankur Verma <ankverma72@gmail.com>
2026-06-23 21:26:06 +0530

    admin page completed

---
+++ app/admin/page.tsx
@@ -0,0 +1,8 @@
+import AdminLeftPanel from '../components/AdminLeftPanel';
+export default function admin(){
+    return (
+        <div>
+                <AdminLeftPanel />
+        </div>
+    )
+}
